845e53ad6eef18fc2a492f9ae6efca5db2585816
tarukosu <4415085+tarukosu@users.noreply.github.com>
2021-01-16 11:33:35 +0900

    Add examples (#4)

--- examples/fire_object/src/lib.rs
+++ examples/fire_object/src/lib.rs
@@ -4,6 +4,50 @@ use once_cell::sync::Lazy;
 use spirare::transform::Transform;
 use spirare::Vector3;
 
+use spirare::element;
+/*
+use element::Element;
+use transform::Transform;
+use uni_wasm::element;
+use uni_wasm::physics;
+use uni_wasm::{transform, Vector3};
+*/
+
+/*
+fn main() {
+    let bullet_index = element::get_resource_index_by_id("123");
+    print!("{}", bullet_index);
+}
+*/
+
+#[no_mangle]
+unsafe fn on_use() {
+    let transform = Transform::myself();
+    let gun_position = transform.world_position();
+    let gun_rotation = transform.world_rotation();
+
+    // spawn bullet
+    let result = element::spawn_object_by_id("bullet");
+    if result.is_err() {
+        return;
+    }
+    let bullet_element = result.unwrap();
+
+    // set position and rotation
+    let offset = 0.5;
+    let forward = transform.forward();
+    let bullet_position = gun_position + offset * forward;
+
+    let bullet_transform = bullet_element.transform();
+    bullet_transform.set_world_position(bullet_position);
+    bullet_transform.set_world_rotation(gun_rotation);
+
+    // set velocity
+    let speed = 4.0;
+    let velocity = speed * forward;
+    bullet_element.physics().set_world_velocity(velocity);
+}
+/*
 #[derive(Clap)]
 struct Opts {
     #[clap(short, long)]
@@ -60,3 +104,4 @@ fn update() {
         }
     }
 }
+*/
